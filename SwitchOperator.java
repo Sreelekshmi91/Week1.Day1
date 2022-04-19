444f8ad1a52cb4e85bb4f78159c531e567e0abe4
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-04-19 16:49:21 +0530

    Add files via upload

---
+++ SwitchOperator.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class SwitchOperator {
+
+	
+	public static void main(String[] args) {
+		int a = 6, b = 3, sum, sub, mul, div;
+		sum = a + b;
+		sub = a - b;
+		mul = a * b;
+		div = a / b;
+		System.out.println("Addition of two Number:" +" " + sum);
+		System.out.println("Subtraction of two Number:" +" " + sub);
+
+		System.out.println("Multiplication of two Number:" +" " + mul);
+
+		System.out.println("Division of two Number:" +" " + div);
+
+
+	}
+}
