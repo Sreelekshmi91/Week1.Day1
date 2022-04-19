07ab53abf2c5a68fa25b2496a9e77b13f45888a4
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-04-19 16:03:27 +0530

    Add files via upload

---
+++ NegativetoPositiveNumber.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class NegativetoPositiveNumber {
+	public static void main(String[] args) {
+        int a=-25;
+        if(a<0) {
+        	int p = a * -1;
+        	System.out.println(p);
+        }
+	}
+}
