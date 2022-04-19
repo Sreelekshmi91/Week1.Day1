0a6f18fbd14c874ac5d1a3afc630a91edcd12e87
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-04-19 16:58:33 +0530

    Update NegativetoPositiveNumber.java

--- NegativetoPositiveNumber.java
+++ NegativetoPositiveNumber.java
@@ -2,6 +2,7 @@ package week1.day1;
 
 public class NegativetoPositiveNumber {
 	public static void main(String[] args) {
+		// Convert negative number into positive number
         int a=-25;
         if(a<0) {
         	int p = a * -1;
