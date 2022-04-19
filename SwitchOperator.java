244916cc08c52bf53e657178fb3ad7cf7c1811d6
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-04-19 16:53:24 +0530

    Update SwitchOperator.java

--- SwitchOperator.java
+++ SwitchOperator.java
@@ -4,6 +4,7 @@ public class SwitchOperator {
 
 	
 	public static void main(String[] args) {
+		
 		//Perform calculation based on the user input operations
 		
 		int a = 6, b = 3, sum, sub, mul, div;
@@ -11,13 +12,11 @@ public class SwitchOperator {
 		sub = a - b;
 		mul = a * b;
 		div = a / b;
+		
 		System.out.println("Addition of two Number:" +" " + sum);
 		System.out.println("Subtraction of two Number:" +" " + sub);
-
 		System.out.println("Multiplication of two Number:" +" " + mul);
-
 		System.out.println("Division of two Number:" +" " + div);
-
 
 	}
 }
