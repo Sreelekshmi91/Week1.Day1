560a2d832b1512887b39857c81742c5d7cb4b540
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-04-19 17:00:07 +0530

    Update CheckPositiveOrNegativeNumber.java

--- CheckPositiveOrNegativeNumber.java
+++ CheckPositiveOrNegativeNumber.java
@@ -2,6 +2,8 @@ package week1.day1;
 
 public class CheckPositiveOrNegativeNumber {
 	public static void main (String[] args) {
+		
+		// To Check Whether a Number is Positive or Negative
 		int n = -75;
 		if (n < 0)
 			System.out.println(n+ " is a negative number");
